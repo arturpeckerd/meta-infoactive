3c5db62ec81395a25bfd5484eff223794d5041d4
Carlos Garcia <carlos.garcia@freescale.com>
2017-02-26 18:22:04 -0600

    removing unecessary files

--- recipes-dctech/gst-infoactive/files/workspace/fbset/chromakey/chromakey.d
+++
@@ -1,8 +0,0 @@
-./chromakey.o chromakey.d:  chromakey.c \
- /home/b42893/yoctos/imx-3.14.28-1.0.0_ga/build-fb-imx6qsabreauto-hard/tmp/sysroots/imx6qsabreauto/usr/src/kernel/include/uapi/linux/mxcfb.h \
- /home/b42893/yoctos/imx-3.14.28-1.0.0_ga/build-fb-imx6qsabreauto-hard/tmp/sysroots/imx6qsabreauto/usr/src/kernel/include/uapi/linux/fb.h \
- /home/b42893/yoctos/imx-3.14.28-1.0.0_ga/build-fb-imx6qsabreauto-hard/tmp/sysroots/imx6qsabreauto/usr/src/kernel/include/uapi/linux/types.h \
- /home/b42893/yoctos/imx-3.14.28-1.0.0_ga/build-fb-imx6qsabreauto-hard/tmp/sysroots/imx6qsabreauto/usr/src/kernel/include/uapi/linux/posix_types.h \
- /home/b42893/yoctos/imx-3.14.28-1.0.0_ga/build-fb-imx6qsabreauto-hard/tmp/sysroots/imx6qsabreauto/usr/src/kernel/include/uapi/linux/stddef.h \
- /home/b42893/yoctos/imx-3.14.28-1.0.0_ga/build-fb-imx6qsabreauto-hard/tmp/sysroots/imx6qsabreauto/usr/src/kernel/include/linux/compiler.h \
- /home/b42893/yoctos/imx-3.14.28-1.0.0_ga/build-fb-imx6qsabreauto-hard/tmp/sysroots/imx6qsabreauto/usr/src/kernel/include/uapi/linux/i2c.h
